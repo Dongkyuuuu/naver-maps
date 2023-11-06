cbc666f6aab76e4a3f051af06780949cf1b8bb12
Dongkyu Kim <desktop@kakao.com>
2023-11-06 22:57:42 +0900

    build: build release 스크립트 추가 및 빌드 시 에러 해결 (#5)
    * fix: lint 설정 파일 문제 해결
    
    * ci: add release lint test
    
    * fix(docs): docs build error fix
    
    * build: release script 추가
    
    * ci: release publish script command change
    
    * chore: changeset

--- apps/docs/src/components/Marker.tsx
+++ apps/docs/src/components/Marker.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { NaverMarker } from "@naver-maps/react";
 import type { PropsWithChildren } from "react";
 
 export default function Map({ children }: PropsWithChildren) {
   return (
     <NaverMarker latitude={37.51347} longitude={127.041722}>
+      {/* @ts-ignore */}
       {children ?? null}
     </NaverMarker>
   );
